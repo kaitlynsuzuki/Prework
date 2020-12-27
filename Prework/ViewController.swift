5868ae88adeb916f3149d035fe38131b13eea38d
kaitlynsuzuki <kaitlynksuzuki@gmail.com>
2020-12-27 03:02:49 -0800

    Initial Commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Kaitlyn Suzuki on 12/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        print("hello!!")
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //get bill amount
+        //?? 0 is error handling, because if there is an issue with converting the string into a double it becomes 0
+        let bill = Double(billField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        //calculate tip + total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        //update labels
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+    }
+    
+}
+
